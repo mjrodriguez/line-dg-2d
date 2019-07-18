fb33654c46d9c99c422937bab97aaea7b61bbcc0
Rodriguez <rodriguez278@Cornice.llnl.gov>
2019-07-18 15:56:06 -0700

    line dg advection and explicit rk4. modified flux is computed for a general mesh, not necessarily just scaled

---
+++ main.py
@@ -0,0 +1,234 @@
+import numpy as np
+import gl
+import matplotlib.pyplot as plt
+
+from mpl_toolkits.mplot3d import Axes3D
+
+###############################################################
+def GenerateGrid(node, xdom,ydom,numOfElx,numOfEly):
+
+    xmin = xdom[0]; xmax = xdom[1];
+    ymin = ydom[0]; ymax = ydom[1];
+
+    dx = (xmax - xmin)/numOfElx
+    dy = (ymax - ymin)/numOfEly
+
+    x = np.zeros( [numOfElx, node.size] )
+    y = np.zeros( [numOfEly, node.size] )
+
+    for n in range(0,numOfElx):
+        x[ n, : ] = n*dx + node*dx
+
+    for n in range(0,numOfEly):
+        y[ n,: ] = n*dy + node*dy
+    #xx, yy = np.meshgrid(x[0,:], y[0,:])
+
+    uinit = np.zeros([numOfElx, numOfEly, node.size, node.size])
+    for ix in range(0,numOfElx):
+        for iy in range(0, numOfEly):
+            xx, yy = np.meshgrid(x[ix,:],y[iy,:])
+            uinit[ix,iy,:,:] = np.sin(2*np.pi*xx)*np.sin(2*np.pi*yy)
+            # for i in range(0, node.size):
+            #     for j in range(0, node.size):
+            #         uinit[ix,iy,i,j] = np.sin(2*np.pi*x[ix,i])*np.sin(2*np.pi*y[iy,j])
+
+
+
+    return uinit,x,y,dx,dy
+###############################################################
+
+####################################################################
+## Computing Flux and modified Flux unstructured mesh
+####################################################################
+def Flux(beta, U):
+    Fx = beta[0]*U
+    Fy = beta[1]*U
+    return Fx, Fy
+
+def ComputeModifiedFlux(detJ, invJ, beta,U):
+    Fx, Fy = Flux(beta,U)
+    modFlux = np.array([np.transpose(Fx), np.transpose(Fy)])
+
+
+    modFlux = detJ*np.dot(invJ,modFlux)
+    if modFlux.size == 2:
+        modFx = modFlux[0]
+        modFy = modFlux[1]
+    else:
+        modFx = np.transpose(modFlux[0,:])
+        modFy = np.transpose(modFlux[1,:])
+    return modFx, modFy
+
+
+def Upwind(waveSpeed, uL,uR):
+    if (waveSpeed > 0):
+        ustar = uL
+    elif (waveSpeed < 0):
+        ustar = uR
+
+    return ustar
+
+def ComputeRHS(u):
+
+    # Construct RHS of equation
+    for ix in range(0,numOfElx):
+        for iy in range(0,numOfEly):
+            # This loop computes the q = x derivative
+            # sweeps through the rows of each element
+            for i in range(0,order+1):
+                ustar = np.zeros(2)
+
+                # Computing u and flux at the quadrature points
+                U = np.matmul( G, u[ix,iy,i,:] )
+                # Fx = detJ*invJ*Flux(beta1,U)
+                Fx, Fy = ComputeModifiedFlux(detJ, invJ, beta, U)
+                temp = -np.matmul( np.matmul(np.transpose(D),W), Fx)
+
+                #print(temp)
+
+                if (ix + 1 >= numOfElx):
+                    # assuming periodic boundary conditions
+                    ustar[0] = Upwind(beta[0], u[ix-1,iy,i,order], u[ix,iy,i,0])
+                    ustar[1] = Upwind(beta[0], u[ix,iy,i,order], u[0,iy,i,0] )
+                else:
+                    ustar[0] = Upwind(beta[0], u[ix-1,iy,i,order], u[ix,iy,i,0])
+                    ustar[1] = Upwind(beta[0], u[ix,iy,i,order], u[ix+1,iy,i,0])
+
+                temp[0]     -=  ComputeModifiedFlux(detJ, invJ, beta, ustar[0])[0] #detJ*invJ[0,0]*beta1*ustar[0]
+                temp[order] +=  ComputeModifiedFlux(detJ, invJ, beta, ustar[1])[0]
+                # temp = -temp + detJ*invJ[0][0]*beta1*ustar*( G[-1][i] - G[0][i] );
+
+                q[ix,iy,i,:] = np.matmul(invMass,temp)
+
+            # This loop computes the r = y derivative
+            # Sweeps through the columns of each element
+            for j in range(0,order+1):
+                ustar = np.zeros(2)
+                U = np.matmul(G, u[ix,iy,:,j])
+                Fx, Fy = ComputeModifiedFlux(detJ, invJ, beta, U)
+                temp = -np.matmul( np.matmul(np.transpose(D), W), Fy )
+
+                if (iy + 1 >= numOfEly):
+                    # assuming periodic boundary conditions
+                    ustar[0] = Upwind(beta[1], u[ix,iy-1,order,j], u[ix,iy,0,j])
+                    ustar[1] = Upwind(beta[1], u[ix,iy,order,j], u[ix,0,0,j] )
+                else:
+                    ustar[0] = Upwind(beta[1], u[ix,iy-1,order,j], u[ix,iy,0,j] )
+                    ustar[1] = Upwind(beta[1], u[ix,iy,order,j], u[ix,iy+1,0,j] )
+
+                temp[0]     -= ComputeModifiedFlux(detJ, invJ, beta, ustar[0])[1]
+                temp[order] += ComputeModifiedFlux(detJ, invJ, beta, ustar[1])[1]
+
+                r[ix,iy,:,j] = np.matmul(invMass, temp)
+
+    return q,r
+
+def ComputeDt():
+    dnode_min    = np.amin(np.diff(xnode[0]))
+    h_min        = np.amin([dx,dy])
+    waveSpeedMax = np.amax([beta1,beta2])
+
+    dt = cflConst*dnode_min*h_min/waveSpeedMax;
+    return dt;
+
+def RK4(currentTime, uold):
+    # Stage 1
+    q,r = ComputeRHS(uold);
+    R   = -(q/dx+r/dy);
+    k1 = dt*R
+
+    #Stage 2
+    q,r = ComputeRHS(uold + 0.5*k1)
+    R   = -(q/dx+r/dy);
+    k2  = dt*R
+
+    #Stage 3
+    q,r = ComputeRHS(uold + 0.5*k2);
+    R   = -(q/dx+r/dy);
+    k3  = dt*R;
+
+    #Stage 4
+    q,r = ComputeRHS(uold + k3);
+    R   = -(q/dx+r/dy);
+    k4  = dt*R;
+
+    unew = uold + ( k1 + 2*k2 + 2*k3 + k4)/6.0;
+    return unew;
+
+def PlotSolution(x,y,usoln):
+    uplot = np.zeros([xnode[0].size*numOfElx,xnode[0].size*numOfEly])
+
+    ax = Axes3D(fig)
+    for ix in range(0,numOfElx):
+        for iy in range(0,numOfEly):
+            xx, yy = np.meshgrid(x[ix,:],y[iy,:])
+            ax.plot_surface(xx,yy,usoln[ix,iy,:,:], rstride=1, cstride=1,edgecolor='none')
+            #h = plt.contourf(xx,yy,usoln[ix,iy,:,:])
+
+            #uplot[ix*xnode[0].size:(ix+1)*xnode[0].size, iy*xnode[0].size:(iy+1)*xnode[0].size] = usoln[ix,iy,:,:]
+
+
+    plt.pause(0.05)
+
+
+if __name__ == "__main__":
+    # Parameters for simulation
+    order = 5;
+    numOfElx = 5; numOfEly = 5;
+    beta1 = 1; beta2 = 1;
+    beta = np.array([beta1,beta2]);
+    cflConst = 1.0; tmax = 0.5;
+
+    # Nodes and quadrature points
+    xnode = gl.lglnodes(order)
+    xint  = gl.lglnodes(30)
+    W     = np.diag(xint[1])
+    G,D   = gl.lagint(xnode[0], xint[0])
+
+    # Grid "generation" and initial conditiion
+    uold,x,y,dx,dy = GenerateGrid(xnode[0], np.array([0,1]), np.array([0,1]), numOfElx, numOfEly)
+
+    fig = plt.figure()
+    PlotSolution(x,y,uold)
+
+    J = np.diag([dx,dy])
+    invJ = np.linalg.inv(J)
+    detJ = np.linalg.det(J)
+
+
+    # Constructing Local Operators
+    Mass = np.matmul( np.matmul(np.transpose(G), W), G )
+    invMass = np.linalg.inv(Mass)
+    Diff  = np.matmul( np.matmul(np.transpose(D), W), G ) # This only works for linear flux
+
+    # uold = np.ones( (numOfElx, numOfEly, order+1, order+1) )
+    q = np.zeros( (numOfElx, numOfEly, order+1, order+1) )
+    r = np.zeros( (numOfElx, numOfEly, order+1, order+1) )
+
+    # q,r = ComputeRHS(uinit);
+    currentTime = 0;
+
+
+    q,r = ComputeRHS(uold);
+
+    fig = plt.figure()
+
+    while currentTime < tmax:
+        print("time = ", currentTime)
+        dt = ComputeDt()
+        if currentTime + dt > tmax:
+            dt = tmax - currentTime;
+
+        unew = RK4(currentTime, uold)
+
+        uold = unew;
+        currentTime += dt;
+
+        #PlotSolution(x,y,uold)
+
+    PlotSolution(x,y,uold)
+    plt.xlabel('$x$')
+    plt.ylabel('$y$')
+    plt.show()
+    # print("q = ", q)
+    # print("r = ", r)
